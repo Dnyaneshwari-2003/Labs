b17176170b08f1ae15c93b0b9e9b798ec5074aa8
Dnyaneshwari <nikamdnyaneshwari84@gmail.com>
2026-06-02 16:14:56 +0530

    This is my first commit

---
+++ Variable.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+name="Dnyaneshwari"
+age=21
+city="nashik"
+role="Computer Engineer"
+
+echo "my name is $name"
+echo "my age is $age"
+echo "my role is $role"
+
+
+#Take User Input
+
+echo "Enter your name:"
+read name
+
+echo "Welcome $name"
+
+#Add Two Numbers
+
+num1=10
+num2=20
+
+sum=$((num1 + num2))
+echo "Sum =$sum"
+
+#Display System Information
+hostnamme=$(hostname)
+date=$(date)
+
+
+echo "Host Name: $hostname"
+echo "Current Date: $date"
+
+
